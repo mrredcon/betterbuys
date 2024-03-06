1d28856313122db34328139347e24214a473ad9e
Connor McCormick <mrredcon@gmail.com>
2024-03-06 09:52:51 -0600

    Admin can now upload pictures

--- www/admin.php
+++ www/admin.php
@@ -27,5 +27,14 @@
 
 			<input type="submit" value="Add product">
 		</form>
+
+		<hr>
+
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			Select image to upload:<br>
+			<input type="file" name="fileToUpload" id="fileToUpload"><br>
+			<br>
+			<input type="submit" value="Upload Image" name="submit"><br>
+		</form>
 	</body>
 </html>
