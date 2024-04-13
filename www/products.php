f5b05e9d4b79d9ecfef9dbc17e5c87e305414a2a
Connor McCormick <mrredcon@gmail.com>
2024-04-13 15:54:52 -0500

    Add product search functionality server-side

--- www/products.php
+++ www/products.php
@@ -1,11 +1,20 @@
 <?php
+
+$RESULTS_PER_PAGE = 20;
+$page = filter_input(INPUT_GET, 'page', FILTER_DEFAULT, array("options" => array("default" => 0)));
+$offset = $page * $RESULTS_PER_PAGE;
+
+$search_terms = filter_input(INPUT_GET, 'query');
+
 $pdo = require_once 'connect.php';
 
-$sql = 'SELECT a.id, a.name, a.price, b.filepath AS imagePath
-	FROM Product a LEFT JOIN ProductImage b ON a.id = b.productId AND b.priority = 0';
+$sql = 'SELECT a.id, a.name, a.description, a.price, b.filepath AS imagePath
+	FROM Product a LEFT JOIN ProductImage b ON a.id = b.productId AND b.priority = 0
+	WHERE a.name LIKE "%' . $search_terms . '%" OR a.description LIKE "%' . $search_terms . '%"
+	LIMIT ' . $offset . ', ' . $RESULTS_PER_PAGE;
 
 $statement = $pdo->query($sql);
-
+ 
 // get all products
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
